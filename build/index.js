b9fd22f45e1e87f145ca56c23a1539a53a5c247a
Omar Sobhy <omar.ahmed.sobhy.96@gmail.com>
2023-07-06 13:49:33 +0400

    Fix createTag params

--- build/index.js
+++ build/index.js
@@ -318,9 +318,7 @@ class Client {
         return __awaiter(this, void 0, void 0, function* () {
             try {
                 const response = yield this.axios.post(`${this.basePath}/projects/${projectId}/tags`, {
-                    params: {
-                        name,
-                    },
+                    name,
                 });
                 return {
                     type: 'success',
